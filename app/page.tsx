6305394fc879491f89a5ceaf2b1702da3f8619f7
Vincent Damaso <cyreeka2016@gmail.com>
2026-01-19 17:46:13 +0800

    style: Wrote TypeScript code for learning purposes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,23 @@
 import Image from "next/image";
 
 export default function Home() {
+
+  const greeting: string | number = 23
+
+  type User = {
+    id: number
+    userName: string
+  }
+
+  const user: User = {
+    id: 1928,
+    userName: 'Neighbor'
+  }
+
+  function printGreeting(greeting: string): string {
+    return greeting
+  }
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -14,7 +31,7 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Hello Next.js!
+            {user.userName}
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
